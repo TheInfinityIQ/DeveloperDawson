c716e7571a70746a5890548253c4102cb4562004
Dawson K. <46154365+TheInfinityIQ@users.noreply.github.com>
2025-10-28 13:32:26 -0500

    Initial blockout

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,30 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-	<header>
-		<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-		<div class="wrapper">
-			<HelloWorld msg="You did it!" />
-
-			<nav>
+	<header class="flex items-center place-content-between m-4">
+		<div class="flex items-center">
+			<img src="src\assets\profile-photo.jpg" class="aspect-square h-24 rounded-full">
+			<h1 class="font-bold ml-4">Developer Dawson</h1>
+		</div>
+		<div>
+			<nav class="flex gap-4">
 				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/about">About</RouterLink>
+				<RouterLink to="/about" class="border-l pl-4">About</RouterLink>
 			</nav>
 		</div>
-		<h1 class="text-3xl font-bold underline">Hello world!</h1>
 	</header>
-
+	<div class="border-b mb-4"></div>
 	<RouterView />
+	<footer class="h-24 p-4">
+			<nav class="flex items-center h-full gap-4">
+				<a href=""><i>icon LINKEDIN</i></a>
+				<a href=""><i>icon RESUME</i></a>
+				<a href=""><i>icon GITHUB</i></a>
+			</nav>
+	</footer>
 </template>
 
 <style scoped>
-header {
-	line-height: 1.5;
-	max-height: 100vh;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
-nav {
-	width: 100%;
-	font-size: 12px;
-	text-align: center;
-	margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-	color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-	background-color: transparent;
-}
-
-nav a {
-	display: inline-block;
-	padding: 0 1rem;
-	border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-	border: 0;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-
-	nav {
-		text-align: left;
-		margin-left: -1rem;
-		font-size: 1rem;
-
-		padding: 1rem 0;
-		margin-top: 1rem;
-	}
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex flex-col px-4">
+    <div class="border-b py-8">
+      <div class="flex items-center w-full rounded-3xl border bg-black hover:bg-transparent">
+        <p class="text-center w-1/2">Lorem Ipsum</p>
+        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png" class="rounded-r-3xl"></a>
+      </div>
+    </div>
+    <div class="border-b py-8">
+      <div class="flex items-center w-full  rounded-3xl border bg-black hover:bg-transparent">
+        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png" class="rounded-l-3xl"></a>
+        <p class="text-center w-1/2">Lorem Ipsum</p>
+      </div>
+    </div>
+    <div class="border-b py-8">
+      <div class="flex items-center w-full  rounded-3xl border bg-black hover:bg-transparent">
+        <p class="text-center w-1/2">Lorem Ipsum</p>
+        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png" class="rounded-r-3xl"></a>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+</style>
