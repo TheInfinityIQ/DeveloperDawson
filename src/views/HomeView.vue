f42b44d737970ca18948f8b1f192b9c811d6ad6f
Dawson K. <46154365+TheInfinityIQ@users.noreply.github.com>
2025-10-28 13:56:49 -0500

    custom colours init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,25 +4,45 @@
 <template>
   <main class="flex flex-col px-4">
     <div class="border-b py-8">
-      <div class="flex items-center w-full rounded-3xl border bg-black hover:bg-transparent">
-        <p class="text-center w-1/2">Lorem Ipsum</p>
-        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png" class="rounded-r-3xl"></a>
+      <h2 class="text-3xl ml-8 mb-2">Project 1</h2>
+      <div
+        class="flex items-center w-full rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out">
+        <p class="text-center w-1/2 px-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+          scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque leo, quis rhoncus odio
+          condimentum vel. Phasellus et suscipit augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue
+          enim, at vehicula lorem urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare lacus,
+          dignissim ultrices eros purus eu lacus.</p>
+        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png "
+            class="rounded-r-3xl"></a>
       </div>
     </div>
     <div class="border-b py-8">
-      <div class="flex items-center w-full  rounded-3xl border bg-black hover:bg-transparent">
-        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png" class="rounded-l-3xl"></a>
-        <p class="text-center w-1/2">Lorem Ipsum</p>
+      <h2 class="text-3xl mr-8 mb-2 text-right">Project 2</h2>
+      <div
+        class="flex items-center w-full  rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out">
+        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png"
+            class="rounded-l-3xl"></a>
+        <p class="text-center w-1/2 px-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+          scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque leo, quis rhoncus odio
+          condimentum vel. Phasellus et suscipit augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue
+          enim, at vehicula lorem urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare lacus,
+          dignissim ultrices eros purus eu lacus.</p>
       </div>
     </div>
     <div class="border-b py-8">
-      <div class="flex items-center w-full  rounded-3xl border bg-black hover:bg-transparent">
-        <p class="text-center w-1/2">Lorem Ipsum</p>
-        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png" class="rounded-r-3xl"></a>
+      <h2 class="text-3xl ml-8 mb-2">Project 3</h2>
+      <div
+        class="flex items-center w-full  rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out">
+        <p class="text-center w-1/2 px-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+          scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque leo, quis rhoncus odio
+          condimentum vel. Phasellus et suscipit augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue
+          enim, at vehicula lorem urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare lacus,
+          dignissim ultrices eros purus eu lacus.</p>
+        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png"
+            class="rounded-r-3xl"></a>
       </div>
     </div>
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
