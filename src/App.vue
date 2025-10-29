85c76b6e29907ae2ac710ceda0bab45507d5b19b
Dawson K. <46154365+TheInfinityIQ@users.noreply.github.com>
2025-10-29 13:39:19 -0500

    Increased header font size

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import { RouterLink, RouterView } from 'vue-router';
 <template>
 	<header class="flex items-center place-content-between m-4">
 		<div class="flex items-center">
-			<img src="src\assets\profile-photo.jpg" class="aspect-square h-24 rounded-full">
-			<h1 class="font-bold ml-4">Developer Dawson</h1>
+			<img src="src\assets\profile-photo.jpg" class="aspect-square h-24 rounded-full" />
+			<h1 class="font-bold ml-4 text-5xl">Developer Dawson</h1>
 		</div>
 		<div>
 			<nav class="flex gap-4">
@@ -18,13 +18,12 @@ import { RouterLink, RouterView } from 'vue-router';
 	<div class="border-b mb-4"></div>
 	<RouterView />
 	<footer class="h-24 p-4">
-			<nav class="flex items-center h-full gap-4">
-				<a href=""><i>icon LINKEDIN</i></a>
-				<a href=""><i>icon RESUME</i></a>
-				<a href=""><i>icon GITHUB</i></a>
-			</nav>
+		<nav class="flex items-center h-full gap-4">
+			<a href=""><i>icon LINKEDIN</i></a>
+			<a href=""><i>icon RESUME</i></a>
+			<a href=""><i>icon GITHUB</i></a>
+		</nav>
 	</footer>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
