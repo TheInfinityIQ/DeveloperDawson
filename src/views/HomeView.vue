6b346d5f79b4512dfab5201c0736e4320344b3ae
Dawson K. <46154365+TheInfinityIQ@users.noreply.github.com>
2025-10-29 13:39:59 -0500

    Added section headers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,127 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main class="flex flex-col px-4">
-    <div class="border-b py-8">
-      <h2 class="text-3xl ml-8 mb-2">Project 1</h2>
-      <div
-        class="flex items-center w-full rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out">
-        <p class="text-center w-1/2 px-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
-          scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque leo, quis rhoncus odio
-          condimentum vel. Phasellus et suscipit augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue
-          enim, at vehicula lorem urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare lacus,
-          dignissim ultrices eros purus eu lacus.</p>
-        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png "
-            class="rounded-r-3xl"></a>
-      </div>
-    </div>
-    <div class="border-b py-8">
-      <h2 class="text-3xl mr-8 mb-2 text-right">Project 2</h2>
-      <div
-        class="flex items-center w-full  rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out">
-        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png"
-            class="rounded-l-3xl"></a>
-        <p class="text-center w-1/2 px-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
-          scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque leo, quis rhoncus odio
-          condimentum vel. Phasellus et suscipit augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue
-          enim, at vehicula lorem urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare lacus,
-          dignissim ultrices eros purus eu lacus.</p>
-      </div>
-    </div>
-    <div class="border-b py-8">
-      <h2 class="text-3xl ml-8 mb-2">Project 3</h2>
-      <div
-        class="flex items-center w-full  rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out">
-        <p class="text-center w-1/2 px-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
-          scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque leo, quis rhoncus odio
-          condimentum vel. Phasellus et suscipit augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue
-          enim, at vehicula lorem urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare lacus,
-          dignissim ultrices eros purus eu lacus.</p>
-        <a href="" class="flex place-content-center w-1/2"><img src="src\assets\portfolio-image-temp.png"
-            class="rounded-r-3xl"></a>
-      </div>
-    </div>
-  </main>
+	<main class="flex flex-col px-4 bg-[--secondary]">
+		<!-- ------------------------------ -->
+		<!-- ---------- My Story ---------- -->
+		<!-- ------------------------------ -->
+		<div>
+			<h2 class="text-3xl">My Story</h2>
+		</div>
+		<div class="flex flex-col">
+			<div class="flex flex-col">
+				<div class="flex items-center gap-2">
+					<i>icon PERSON</i>
+					<h3 class="text-2xl">About</h3>
+				</div>
+				<div>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+						scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend
+						pellentesque leo, quis rhoncus odio condimentum vel. Phasellus et suscipit
+						augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue enim
+					</p>
+				</div>
+			</div>
+			<div class="flex flex-col">
+				<div class="flex items-center gap-2">
+					<i>icon PAPER</i>
+					<h3 class="text-2xl">Experience</h3>
+				</div>
+				<div>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+						scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend
+						pellentesque leo, quis rhoncus odio condimentum vel. Phasellus et suscipit
+						augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue enim
+					</p>
+				</div>
+			</div>
+			<div class="flex flex-col">
+				<div class="flex items-center gap-2">
+					<i>icon GRADUATION</i>
+					<h3 class="text-2xl">Education</h3>
+				</div>
+				<div>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+						scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend
+						pellentesque leo, quis rhoncus odio condimentum vel. Phasellus et suscipit
+						augue. Integer laoreet, orci sit amet dapibus varius, nunc diam congue enim
+					</p>
+				</div>
+			</div>
+		</div>
+		<!-- ------------------------------ -->
+		<!-- ---------- Projects ---------- -->
+		<!-- ------------------------------ -->
+		<div>
+			<h2 class="text-3xl">Projects</h2>
+		</div>
+		<div class="border-b">
+			<h3 class="text-2xl ml-8 mb-2">Project 1</h3>
+			<div
+				class="flex items-center w-full rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out"
+			>
+				<a href="" class="flex place-content-center w-1/2"
+					><img src="src\assets\portfolio-image-temp.png " class="rounded-r-3xl"
+				/></a>
+				<p class="text-center w-1/2 px-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+					scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque
+					leo, quis rhoncus odio condimentum vel. Phasellus et suscipit augue. Integer
+					laoreet, orci sit amet dapibus varius, nunc diam congue enim, at vehicula lorem
+					urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare
+					lacus, dignissim ultrices eros purus eu lacus.</p
+				>
+			</div>
+		</div>
+		<div class="border-b">
+			<h2 class="text-2xl mr-8 mb-2 text-right">Project 2</h2>
+			<div
+				class="flex items-center w-full rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out"
+			>
+				<p class="text-center w-1/2 px-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+					scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque
+					leo, quis rhoncus odio condimentum vel. Phasellus et suscipit augue. Integer
+					laoreet, orci sit amet dapibus varius, nunc diam congue enim, at vehicula lorem
+					urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare
+					lacus, dignissim ultrices eros purus eu lacus.</p
+				>
+				<a href="" class="flex place-content-center w-1/2"
+					><img src="src\assets\portfolio-image-temp.png" class="rounded-l-3xl"
+				/></a>
+			</div>
+		</div>
+		<div class="border-b">
+			<h2 class="text-2xl ml-8 mb-2">Project 3</h2>
+			<div
+				class="flex items-center w-full rounded-3xl border bg-black hover:bg-transparent transition duration-500 ease-out"
+			>
+				<a href="" class="flex place-content-center w-1/2"
+					><img src="src\assets\portfolio-image-temp.png" class="rounded-r-3xl"
+				/></a>
+				<p class="text-center w-1/2 px-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo orci,
+					scelerisque quis eros in, accumsan pulvinar ipsum. Quisque eleifend pellentesque
+					leo, quis rhoncus odio condimentum vel. Phasellus et suscipit augue. Integer
+					laoreet, orci sit amet dapibus varius, nunc diam congue enim, at vehicula lorem
+					urna a sem. Maecenas ornare, metus eu faucibus iaculis, justo lacus ornare
+					lacus, dignissim ultrices eros purus eu lacus.</p
+				>
+			</div>
+		</div>
+		<!-- ------------------------------ -->
+		<!-- ---------- Stim Bin ---------- -->
+		<!-- ------------------------------ -->
+		<div>
+			<h2 class="text-3xl">Stim Bin</h2>
+		</div>
+		<div>
+			<!-- Objects in toy bin -->
+		</div>
+	</main>
 </template>
 
 <style scoped></style>
